eef758dd764deadd6956fe31a0b79ff6555b70ba
Juan Pablo Giraldo Giraldo <juan.giraldo@bizagi.com>
2016-01-31 20:47:45 -0500

    Taller 1 - Punto 1

--- client-test.js
+++ client-test.js
@@ -1,15 +1,14 @@
 var seneca = require('seneca')()
 var client = seneca.client({ host:'localhost', port:8080 })
 
-var number1 = 1;
-var number2 = 2;
+var numbers = [1, 2, 5];
 
 // Envia al topico de la suma los operandos y espera respuesta
-client.act({ type: 'calculadora', function: 'sumar', vars:{"num1":number1,"num2":number2}}, function (err, result) {
+client.act({ type: 'calculadora', function: 'sumar', vars:{"nums": numbers}}, function (err, result) {
   console.log(result);
 })
 
 // Envia al topico de la resta los operandos y espera respuesta
-client.act({ type: 'calculadora', function: 'restar', vars:{"num1":number1,"num2":number2}}, function (err, result) {
+client.act({ type: 'calculadora', function: 'restar', vars:{"nums":numbers}}, function (err, result) {
   console.log(result);
 })
--- microservice.js
+++ microservice.js
@@ -4,17 +4,25 @@ module.exports = function service(options) {
   this.add({ type: 'calculadora', function: 'restar' }, restar)
 
   function sumar(msg, respond) {
-    var vars = msg.vars;
-    var num1 = vars.num1;
-    var num2 = vars.num2;
-    respond( null, { respond: num1+num2 });
+    var nums = msg.vars.nums;
+    var result = 0;
+    
+    nums.forEach(function(num) {
+        result += num;    
+    }, this);
+    
+    respond( null, { respond: result });
   }
 
   function restar(msg, respond) {
-      var vars = msg.vars;
-      var num1 = vars.num1;
-      var num2 = vars.num2;
-      respond( null, { respond: num1-num2 });
+     var nums = msg.vars.nums;
+     var result = 0;
+        
+     nums.forEach(function(num) {
+        result -= num;    
+     }, this);
+        
+     respond( null, { respond: result });
   }
 
 }
